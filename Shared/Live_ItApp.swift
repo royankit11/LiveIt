111c72a3c28f2765c5863eca6a85994a2d1c30e2
Rik Roy <royankit11@gmail.com>
2021-07-22 10:22:04 -0400

    Initial Commit

---
+++ Shared/Live_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Live_ItApp.swift
+//  Shared
+//
+//  Created by Rik Roy on 7/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Live_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
